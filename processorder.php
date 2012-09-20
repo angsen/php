b788b1ac1695b628c8bef102a50e4308dfd65883
angsen <angseni@gmail.com>
2012-09-21 00:08:00 +0900

    from windows7

---
+++ processorder.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Bob's Auto Parts - Order Results</title>
+</head>
+<body> <h1>Bob's Auto Parts</h1> <h2>Order Results</h2>
+<?php
+	echo '<p>Order processed.</p>';
+
+	echo "<p>Order processed at ";
+	echo date('H:i, js F Y');
+	echo "</p>";
+
+	echo "<p>Order processed at ".date('H:i, js F Y')."</p>";
+
+	$tireqty = $_POST['tireqty'];
+	$oilqty = $_POST['oilqty'];
+	$sparkqty = $_POST['sparkqty'];
+
+	echo '<p>Your order is as follows: </p>';
+	echo $tireqty.' tires<br />';
+	echo $oilqty.' bottles of oil<br />';
+	echo $sparkqty.' spark plugs<br />';
+
+	
+?>
+	
+</body>
+</html>
